f4527f9bbb6cf4791666c9e128724284a004aff0
Chase McClure <chase@studioofthemonth.com>
2016-08-08 14:51:57 -0500

    test condition

--- Ditto/Ditto.swift
+++ Ditto/Ditto.swift
@@ -17,7 +17,7 @@ public class Ditto {
     private static var configuration: DittoConfiguration!
     private static var layoutSheet: DittoSheet = [:]
     
-    private let keys: [ String ]
+    private var keys: [ String ]!
     private weak var view: UIView!
     private var views: [ String: UIView ] = [:]
     private var blocks: [[ DittoBlock ]] = []
@@ -34,27 +34,27 @@ public class Ditto {
     ) {
         var keys = keys
         var filteredViews: [ String: UIView ] = [:]
-        
+
         if key != nil {
             keys.append(key!)
         }
-        
+
         for (key, view) in views {
             if let v = view {
                 filteredViews[key] = v
             }
         }
-        
+
         self.keys = keys
         self.view = view
-        
+
         // add blocks from sheets
         for key in keys {
             if let b = Ditto.layoutSheet[key] {
                 blocks.append(b)
             }
         }
-        
+
         // add additional blocks
         for b in additionalBlocks {
             blocks.append(b)
@@ -73,13 +73,13 @@ public class Ditto {
         for (_, v) in filteredViews {
             v.translatesAutoresizingMaskIntoConstraints = false
         }
-        
+
         // set super view
         filteredViews["super"] = view
         self.views = filteredViews
         
-        //        DittoTracker.open(key)
-        
+        // DittoTracker.open(key)
+
         // prepare for auto update
         registerDimensionsDidChangeNotification()
         
@@ -87,138 +87,138 @@ public class Ditto {
     
     deinit {
         //        DittoTracker.close(key)
-        view = nil
-        views = [:]
-        currentConstraints = []
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+//        view = nil
+//        views = [:]
+//        currentConstraints = []
+//        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
     public func set(immediate: Bool = false) {
-        removeConstraints()
-        
-        for group in blocks {
-            if let block = getMatchedBlock(group) {
-                
-                if !block.constraints.isEmpty {
-                    currentConstraints.appendContentsOf(block.constraints)
-                    view.addConstraints(block.constraints)
-                }
-                
-                for string in block.strings {
-                    
-                    // CenterX:
-                    if string.containsString("CenterX:") {
-                        addCenterXConstraint(string)
-                    }
-                    // CenterY:
-                    else if string.containsString("CenterY:") {
-                        addCenterYConstraint(string)
-                    }
-                    // Hide:
-                    else if string.containsString("Hide:") {
-                        hideView(string)
-                    }
-                    // Show:
-                    else if string.containsString("Show:") {
-                        showView(string)
-                    }
-                    // VFL
-                    else {
-                        addVFLConstraint(string)
-                    }
-                    
-                }
-            }
-        }
-        
-        if (immediate) {
-            view.setNeedsLayout()
-            view.layoutIfNeeded()
-        }
+//        removeConstraints()
+//        
+//        for group in blocks {
+//            if let block = getMatchedBlock(group) {
+//                
+//                if !block.constraints.isEmpty {
+//                    currentConstraints.appendContentsOf(block.constraints)
+//                    view.addConstraints(block.constraints)
+//                }
+//                
+//                for string in block.strings {
+//                    
+//                    // CenterX:
+//                    if string.containsString("CenterX:") {
+//                        addCenterXConstraint(string)
+//                    }
+//                    // CenterY:
+//                    else if string.containsString("CenterY:") {
+//                        addCenterYConstraint(string)
+//                    }
+//                    // Hide:
+//                    else if string.containsString("Hide:") {
+//                        hideView(string)
+//                    }
+//                    // Show:
+//                    else if string.containsString("Show:") {
+//                        showView(string)
+//                    }
+//                    // VFL
+//                    else {
+//                        addVFLConstraint(string)
+//                    }
+//                    
+//                }
+//            }
+//        }
+//        
+//        if (immediate) {
+//            view.setNeedsLayout()
+//            view.layoutIfNeeded()
+//        }
         
     }
     
     private func addCenterXConstraint(string: String) {
-        var string = String(string.characters.dropFirst(9))
-        string = String(string.characters.dropLast(2))
-        
-        let views = string.characters.split{$0 == "["}.map(String.init)
-        
-        if self.views[views[1]] == nil {
-            print("CenterX: Constraint can't be set. Missing inner view \(views[1])")
-            return
-        }
-        
-        if self.views[views[0]] == nil {
-            print("CenterX: Constraint can't be set. Missing outer view \(views[0])")
-            return
-        }
-        
-        let c = NSLayoutConstraint(
-            item: self.views[views[1]]!,
-            attribute: .CenterX,
-            relatedBy: .Equal,
-            toItem: self.views[views[0]],
-            attribute: .CenterX,
-            multiplier: 1,
-            constant: 0)
-        
-        currentConstraints.append(c)
-        view.addConstraint(c)
+//        var string = String(string.characters.dropFirst(9))
+//        string = String(string.characters.dropLast(2))
+//        
+//        let views = string.characters.split{$0 == "["}.map(String.init)
+//        
+//        if self.views[views[1]] == nil {
+//            print("CenterX: Constraint can't be set. Missing inner view \(views[1])")
+//            return
+//        }
+//        
+//        if self.views[views[0]] == nil {
+//            print("CenterX: Constraint can't be set. Missing outer view \(views[0])")
+//            return
+//        }
+//        
+//        let c = NSLayoutConstraint(
+//            item: self.views[views[1]]!,
+//            attribute: .CenterX,
+//            relatedBy: .Equal,
+//            toItem: self.views[views[0]],
+//            attribute: .CenterX,
+//            multiplier: 1,
+//            constant: 0)
+//        
+//        currentConstraints.append(c)
+//        view.addConstraint(c)
     }
     
     private func addCenterYConstraint(string: String) {
-        var string = String(string.characters.dropFirst(9))
-        string = String(string.characters.dropLast(2))
-        
-        let views = string.characters.split{$0 == "["}.map(String.init)
-        
-        if self.views[views[1]] == nil {
-            print("CenterY: Constraint can't be set. Missing inner view \(views[1])")
-            return
-        }
-        
-        if self.views[views[0]] == nil {
-            print("CenterY: Constraint can't be set. Missing outer view \(views[0])")
-            return
-        }
-        
-        let c = NSLayoutConstraint(
-            item: self.views[views[1]]!,
-            attribute: .CenterY,
-            relatedBy: .Equal,
-            toItem: self.views[views[0]]!,
-            attribute: .CenterY,
-            multiplier: 1,
-            constant: 0)
-        
-        currentConstraints.append(c)
-        view.addConstraint(c)
+//        var string = String(string.characters.dropFirst(9))
+//        string = String(string.characters.dropLast(2))
+//        
+//        let views = string.characters.split{$0 == "["}.map(String.init)
+//        
+//        if self.views[views[1]] == nil {
+//            print("CenterY: Constraint can't be set. Missing inner view \(views[1])")
+//            return
+//        }
+//        
+//        if self.views[views[0]] == nil {
+//            print("CenterY: Constraint can't be set. Missing outer view \(views[0])")
+//            return
+//        }
+//        
+//        let c = NSLayoutConstraint(
+//            item: self.views[views[1]]!,
+//            attribute: .CenterY,
+//            relatedBy: .Equal,
+//            toItem: self.views[views[0]]!,
+//            attribute: .CenterY,
+//            multiplier: 1,
+//            constant: 0)
+//        
+//        currentConstraints.append(c)
+//        view.addConstraint(c)
     }
     
     private func hideView(string: String) {
-        var string = String(string.characters.dropFirst(6))
-        string = String(string.characters.dropLast(1))
-        
-        views[string]?.hidden = true
+//        var string = String(string.characters.dropFirst(6))
+//        string = String(string.characters.dropLast(1))
+//        
+//        views[string]?.hidden = true
     }
     
     private func showView(string: String) {
-        var string = String(string.characters.dropFirst(6))
-        string = String(string.characters.dropLast(1))
-        
-        views[string]?.hidden = false
+//        var string = String(string.characters.dropFirst(6))
+//        string = String(string.characters.dropLast(1))
+//        
+//        views[string]?.hidden = false
     }
     
     private func addVFLConstraint(string: String) {
-        let c = NSLayoutConstraint.constraintsWithVisualFormat(
-            string,
-            options: [],
-            metrics: nil,
-            views: views )
-        
-        currentConstraints.appendContentsOf(c)
-        view.addConstraints(c)
+//        let c = NSLayoutConstraint.constraintsWithVisualFormat(
+//            string,
+//            options: [],
+//            metrics: nil,
+//            views: views )
+//        
+//        currentConstraints.appendContentsOf(c)
+//        view.addConstraints(c)
     }
     
     
@@ -227,16 +227,16 @@ public class Ditto {
     
     private func getMatchedBlock(blocks: [ DittoBlock ]) -> DittoBlock? {
         var block: DittoBlock!
-        var score = -1
-        
-        for b in blocks {
-            let s = b.match()
-            
-            if s > score {
-                block = b
-                score = s
-            }
-        }
+//        var score = -1
+//        
+//        for b in blocks {
+//            let s = b.match()
+//            
+//            if s > score {
+//                block = b
+//                score = s
+//            }
+//        }
         
         return block
     }
@@ -246,23 +246,23 @@ public class Ditto {
     // MARK: Remove Constraints
     
     public func removeConstraints() {
-        for c in currentConstraints {
-            c.active = false
-        }
-        
-        currentConstraints = []
+//        for c in currentConstraints {
+//            c.active = false
+//        }
+//        
+//        currentConstraints = []
     }
     
     private func registerDimensionsDidChangeNotification() {
-        NSNotificationCenter.defaultCenter().addObserver(
-            self,
-            selector: #selector(onDimensionsDidChange),
-            name: DittoDimensionsDidChange,
-            object: nil)
+//        NSNotificationCenter.defaultCenter().addObserver(
+//            self,
+//            selector: #selector(onDimensionsDidChange),
+//            name: DittoDimensionsDidChange,
+//            object: nil)
     }
     
     @objc private func onDimensionsDidChange() {
-        set(true)
+//        set(true)
     }
     
     
@@ -271,15 +271,15 @@ public class Ditto {
      * Cleans up an array of views that have layouts
      */
     public static func cleanUp(views: AnyObject) {
-        if let v = views as? [ DittoLayoutable? ] {
-            for view in v {
-                view?.teardownLayout()
-            }
-        }
+//        if let v = views as? [ DittoLayoutable? ] {
+//            for view in v {
+//                view?.teardownLayout()
+//            }
+//        }
     }
     
     public static func cleanUp(view: DittoLayoutable?) {
-        view?.teardownLayout()
+//        view?.teardownLayout()
     }
     
     
@@ -340,12 +340,12 @@ public class Ditto {
      */
     public static func Universal(
         width width: SizeClass? = nil,
-              height: SizeClass? = nil,
-              widths: [ SizeClass ] = [],
-              heights: [ SizeClass ] = [],
-              constraints: [ NSLayoutConstraint ] = [],
-              strings: [ String ] = []
-        ) -> DittoBlock {
+          height: SizeClass? = nil,
+          widths: [ SizeClass ] = [],
+          heights: [ SizeClass ] = [],
+          constraints: [ NSLayoutConstraint ] = [],
+          strings: [ String ] = []
+    ) -> DittoBlock {
         var widths = widths
         var heights = heights
         
